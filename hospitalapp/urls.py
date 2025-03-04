d9ff0260680a3eae29f31c9b71e1cbbd361bfc90
abedmumo03@gmail.com <clitquefine@gmail.com>
2025-03-04 16:16:27 +0300

    initial commit

--- hospitalapp/urls.py
+++ hospitalapp/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path('service/', views.service, name='service'),
     path('department/', views.department, name='department'),
     path('doctor/', views.doctor, name='doctor'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('contact/', views.contact, name='contact'),
+    path('show/', views.show, name='show'),
+    path('delete/<int:id>', views.delete, name='show'),
+    path('edit/<int:id>', views.edit, name='edit'),
 
 ]
--- hospitalapp/views.py
+++ hospitalapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from hospitalapp.models import *
 
 # Create your views here.
 def index(request):
@@ -16,3 +17,61 @@ def department(request):
 
 def doctor(request):
     return render(request, 'doctor.html')
+
+def appointment(request):
+    if request.method == "POST":
+        myappointment = Appointment(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            phone = request.POST['phone'],
+            date= request.POST['date'],
+            department = request.POST['department'],
+            doctor= request.POST['doctor'],
+            message = request.POST['message'],
+        )
+        myappointment.save()
+        return redirect('/show')
+
+    else:
+        return render (request, 'appointment.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        mycontact = Contact(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            subject = request.POST['subject'],
+            message = request.POST['message'],
+        )
+        mycontact.save()
+        return redirect('/contact')
+
+    else:
+        return render (request, 'contact.html')
+
+
+def show(request):
+    all = Appointment.objects.all()
+
+    return render(request, 'show.html',{'all':all})
+
+def delete(request,id):
+    deletedappointment = Appointment.objects.get(id=id)
+    deletedappointment.delete()
+    return redirect('/show')
+
+def edit(request,id):
+    editinfo = get_object_or_404(Appointment, id = id)
+    if request.method =='POST':
+        editinfo.name = request.POST.get('name')
+        editinfo.email = request.POST.get('email')
+        editinfo.phone = request.POST.get('phone')
+        editinfo.date = request.POST.get('date')
+        editinfo.department = request.POST.get('department')
+        editinfo.doctor = request.POST.get('doctor')
+        editinfo.message = request.POST.get('message')
+        editinfo.save()
+        return redirect('/show')
+    else:
+        return render(request, 'edit.html', {'editinfo': editinfo})
